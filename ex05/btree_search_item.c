d5d8fdb9851093b6be60a77aa311a4e34b72d7ca
bchaleil <Baptiste.chaleil@gmail.com>
2015-12-20 13:43:03 +0100

    rendu05

---
+++ ex05/btree_search_item.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   btree_search_item.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bchaleil <bchaleil@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2015/12/20 12:49:28 by bchaleil          #+#    #+#             */
+/*   Updated: 2015/12/20 13:29:40 by bchaleil         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_btree.h"
+
+void	*btree_search_item(t_btree *root, void *data_ref,
+		int (*cmpf)(void *, void *))
+{
+	void *result;
+
+	result = NULL;
+	if (root == NULL)
+		return (NULL);
+	if (root->left != NULL)
+		result = btree_search_item(root->left, data_ref, cmpf);
+	if (cmpf(root->item, data_ref) == 0)
+		return (root->item);
+	if (result == NULL && root->right != NULL)
+		result = btree_search_item(root->right, data_ref, cmpf);
+	return (result);
+}
